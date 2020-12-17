31042599cce9687a1b64369147b91e534aeff02a
Paul Spende <paul.spende@gmail.com>
2020-12-17 15:50:40 +0100

    abort more types of requests

--- utils/interceptors.js
+++ utils/interceptors.js
@@ -1,9 +1,41 @@
+const blockedResourceTypes = [
+  "image",
+  "media",
+  "font",
+  "texttrack",
+  "object",
+  "beacon",
+  "csp_report",
+  "imageset",
+];
+
+const skippedResources = [
+  "quantserve",
+  "adzerk",
+  "doubleclick",
+  "adition",
+  "exelator",
+  "sharethrough",
+  "cdn.api.twitter",
+  "google-analytics",
+  "googletagmanager",
+  "google",
+  "fontawesome",
+  "facebook",
+  "analytics",
+  "optimizely",
+  "clicktale",
+  "mixpanel",
+  "zedo",
+  "clicksor",
+  "tiqcdn",
+];
+
 exports.handleRequests = (interceptedRequest) => {
-  const url = interceptedRequest.url();
+  const requestUrl = interceptedRequest._url.split("?")[0].split("#")[0];
   if (
-    interceptedRequest.resourceType() === "stylesheet" ||
-    interceptedRequest.resourceType() === "font" ||
-    interceptedRequest.resourceType() === "image"
+    blockedResourceTypes.indexOf(interceptedRequest.resourceType()) !== -1 ||
+    skippedResources.some((resource) => requestUrl.indexOf(resource) !== -1)
   ) {
     interceptedRequest.abort();
   } else {
