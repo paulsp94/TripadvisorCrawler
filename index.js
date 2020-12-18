e29f6425ac9116c03b6411732d1ed093ba14aba6
Paul Spende <paul.spende@gmail.com>
2020-12-18 22:34:59 +0100

    rewritten to use puppeteer cluster with stealth/adblocker

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const puppeteer = require("puppeteer");
-const cliProgress = require("cli-progress");
+const puppeteer = require("puppeteer-extra");
+
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+const adblocker = AdblockerPlugin({
+  blockTrackers: true,
+});
+puppeteer.use(adblocker);
+
+const { Cluster } = require("puppeteer-cluster");
+
 const fs = require("fs");
 
-const { handleRequests } = require("./utils/interceptors");
-const { error } = require("console");
+// const { handleRequests } = require("./utils/interceptors");
+
+process.on("unhandledRejection", (error, p) => {
+  console.log("=== UNHANDLED REJECTION ===");
+  console.dir(error);
+});
 
 // Selectors
 const nextButtonSelector = ".nav.next";
@@ -11,132 +26,73 @@ const resultSelector = "[data-test-attribute=typeahead-results] > a";
 const restaurantLinkSelector = 'a[href^="/Restaurant_"]';
 
 // Results
-const results = [];
 let errors = [];
 
 const restaurantUrls = fs
   .readFileSync("data/restaurantsListUrls.txt", "utf8")
   .split("\n");
 restaurantUrls.pop();
-console.log(restaurantUrls.length);
 
 const getRestaurantUrls = (page) =>
   page.$$eval(restaurantLinkSelector, (nodes) =>
     nodes.map((node) => node.href.match(/\/Restaurant_.*.html/g)[0])
   );
 
-const crawlCity = async (page, url) => {
-  const restaurants = new Set();
-  await page
-    .goto(url, { waitUntil: "networkidle0" })
-    .catch((error) => errors.push({ city: url, error, id: 31 }));
-
-  let urls = await getRestaurantUrls(page).catch((error) =>
-    errors.push({ city: url, error, id: 34 })
-  );
-  urls.forEach((url) => restaurants.add(url));
-
-  let nextButtonUrl = await page
-    .$eval(nextButtonSelector, (node) => node.href)
-    .catch((error) => errors.push({ city: url, error, id: 40 }));
-  while (nextButtonUrl) {
-    await page
-      .goto(nextButtonUrl)
-      .catch((error) => errors.push({ city: url, error }));
-    urls = await getRestaurantUrls(page).catch((error) =>
-      errors.push({ city: url, error, id: 46 })
-    );
-    urls.forEach((url) => restaurants.add(url));
-    nextButtonUrl = await page
-      .$eval(nextButtonSelector, (node) => node.href)
-      .catch((error) => errors.push({ city: url, error, id: 51 }));
-  }
-
-  return restaurants;
-};
-
 (async () => {
-  let browser = null;
-  const progressBar = new cliProgress.SingleBar(
-    {},
-    cliProgress.Presets.shades_classic
-  );
-  try {
-    browser = await puppeteer.connect({
-      browserWSEndpoint:
-        "ws://localhost:3000" +
-        "?--window-size=1920x1080" +
-        "&--no-sandbox=true" +
-        "&--shm-size=3gb" +
-        "&--disable-setuid-sandbox=true" +
-        "&--disable-dev-shm-usage=true" +
-        "&--disable-accelerated-2d-canvas=true" +
-        "&--disable-gpu=true",
-    });
-    let page = await browser.newPage();
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_BROWSER,
+    maxConcurrency: 2,
+    monitor: true,
+    timeout: 3600 * 1000,
+    puppeteer,
+    puppeteerOptions: {
+      timeout: 300 * 1000,
+    },
+  });
 
-    await page.setRequestInterception(true);
-    page.on("request", handleRequests);
+  await cluster.task(async ({ page, data: url }) => {
+    await page.goto(url);
+    let restaurants = new Set();
 
-    progressBar.start(restaurantUrls.length, 0);
+    let urls = await getRestaurantUrls(page);
+    urls.forEach((url) => restaurants.add(url));
 
-    let url = restaurantUrls.pop();
-    progressBar.increment();
-    let restaurants = await crawlCity(page, url).catch((error) => {
-      errors.push({ city: url, error, id: 82 });
-      return false;
-    });
-    restaurants && results.push(...restaurants);
+    let nextButtonUrl = await page.$eval(
+      nextButtonSelector,
+      (node) => node.href
+    );
 
-    while (restaurantUrls.length) {
-      try {
-        url = restaurantUrls.pop();
-        progressBar.increment();
-        restaurants = await crawlCity(page, url).catch((error) => {
-          errors.push({ city: url, error, id: 91 });
-          return false;
-        });
-        restaurants && results.push(...restaurants);
-      } catch (err) {
-        console.log("While catch");
-        restaurantUrls.push(url);
-        browser = await puppeteer.connect({
-          browserWSEndpoint:
-            "ws://localhost:3000" +
-            "?--window-size=1920x1080" +
-            "&--no-sandbox=true" +
-            "&--shm-size=3gb" +
-            "&--disable-setuid-sandbox=true" +
-            "&--disable-dev-shm-usage=true" +
-            "&--disable-accelerated-2d-canvas=true" +
-            "&--disable-gpu=true",
-        });
-        let page = await browser.newPage();
-      }
+    while (nextButtonUrl) {
+      await page.goto(nextButtonUrl);
+      urls = await getRestaurantUrls(page);
+      urls.forEach((url) => restaurants.add(url));
+      nextButtonUrl = await page.$eval(nextButtonSelector, (node) => node.href);
     }
 
-    // console.log(results);
-    console.log(results.size);
+    restaurants = [...restaurants];
 
-    const file = fs.createWriteStream("data/restaurantUrls.txt");
+    const file = fs.createWriteStream(
+      `data/RestaurantsInCities/${url.slice(27, -5)}.txt`
+    );
     file.on("error", console.log);
-    results.forEach((item) => {
+    restaurants.forEach((item) => {
       file.write(item + "\n");
     });
     file.end();
+  });
 
-    errors = errors.map((item) => ({ ...item, error: item.error.toString() }));
-    fs.writeFile("data/errors.json", JSON.stringify(errors, null, 4), (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("JSON saved to data/errors.json");
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  } finally {
-    progressBar && progressBar.stop();
-    browser && browser.close();
-  }
+  cluster.on("taskerror", (err, data, willRetry) => {
+    if (willRetry) {
+      console.warn(
+        `Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`
+      );
+    } else {
+      console.error(`Failed to crawl ${data}: ${err.message}`);
+    }
+  });
+
+  restaurantUrls.forEach((url) => cluster.queue(url));
+
+  await cluster.idle();
+  await cluster.close();
 })();
