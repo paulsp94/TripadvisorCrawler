a1e7991cc5fedb0dfab57a149a98f234f3ea2df5
Paul Spende <paul.spende@gmail.com>
2020-12-29 04:29:12 +0100

    work on next crawler

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { Cluster } = require("puppeteer-cluster");
 
 const fs = require("fs");
 
-// const { handleRequests } = require("./utils/interceptors");
+const { initDatabase } = require("./database/mongoConnector");
 
 process.on("unhandledRejection", (error, p) => {
   console.log("=== UNHANDLED REJECTION ===");
@@ -22,33 +22,38 @@ process.on("unhandledRejection", (error, p) => {
 
 // Selectors
 const nextButtonSelector = ".nav.next";
-const resultSelector = "[data-test-attribute=typeahead-results] > a";
-const restaurantLinkSelector = 'a[href^="/Restaurant_"]';
 
-// Results
-let errors = [];
+const nameSelector = '[data-test-target="top-info-header"]';
+const adressSelector = 'a[href="#MAPVIEW"]';
+const phoneSelector = 'a[href^="tel:"]';
+const emailSelector = 'a[href^="mailto:"]';
+const websiteSelector = '//a[text()="Webseite"]';
+const reviewsSelector = 'a[href="#REVIEWS"]';
+const starsSelector = '//div[text()="GESAMTWERTUNGEN"]';
+const priceRangeSelector = '//div[text()="PREISSPANNE"]';
+const kitchenSelector = '//div[text()="KÜCHEN"]';
+const descriptionSelector = '//div[text()="Beschreibung"]';
+const otherDietsSelector = '//div[text()="ANDERE ERNÄHRUNGSFORMEN"]';
+const mealsSelector = '//div[text()="MAHLZEITEN"]';
+const otherFunctionsSelector = '//div[text()="FUNKTIONEN"]';
+const ratingDistributionSelector = 'div[data-param="trating"]';
 
-const restaurantUrls = fs
-  .readFileSync("data/restaurantsListUrls.txt", "utf8")
-  .split("\n");
-restaurantUrls.pop();
-
-const getRestaurantUrls = (page) =>
-  page.$$eval(restaurantLinkSelector, (nodes) =>
-    nodes.map((node) => node.href.match(/\/Restaurant_.*.html/g)[0])
-  );
+const allDetailsSelector = '//a[text()="Alle Details anzeigen"]';
 
 (async () => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_BROWSER,
     maxConcurrency: 2,
     monitor: true,
-    timeout: 3600 * 1000,
+    timeout: 3600 * 2000,
     puppeteer,
     puppeteerOptions: {
-      timeout: 300 * 1000,
+      timeout: 600 * 1000,
+      args: ["--disable-dev-shm-usage"],
     },
   });
+
+  const { insertManyRestaurants } = await initDatabase();
 
   await cluster.task(async ({ page, data: url }) => {
     await page.goto(url);
@@ -69,16 +74,11 @@ const getRestaurantUrls = (page) =>
       nextButtonUrl = await page.$eval(nextButtonSelector, (node) => node.href);
     }
 
-    restaurants = [...restaurants];
+    restaurants = [...restaurants].map((item) => ({
+      url: baseURL + item,
+    }));
 
-    const file = fs.createWriteStream(
-      `data/RestaurantsInCities/${url.slice(27, -5)}.txt`
-    );
-    file.on("error", console.log);
-    restaurants.forEach((item) => {
-      file.write(item + "\n");
-    });
-    file.end();
+    insertManyRestaurants(restaurants);
   });
 
   cluster.on("taskerror", (err, data, willRetry) => {
