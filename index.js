0479f1f21abda1c3950b50a732906a75038f3a42
Paul Spende <paul.spende@gmail.com>
2021-01-15 10:01:34 +0100

    finished crawler

--- index.js
+++ index.js
@@ -1,212 +1,48 @@
-const puppeteer = require("puppeteer-extra");
+import puppeteer from "puppeteer-extra";
 
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+import { enchantPuppeteer } from "enchant-puppeteer";
+enchantPuppeteer();
+
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(StealthPlugin());
 
-const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 const adblocker = AdblockerPlugin({
   blockTrackers: true,
 });
 puppeteer.use(adblocker);
 
-const { Cluster } = require("puppeteer-cluster");
+import blockResourcesPlugin from "puppeteer-extra-plugin-block-resources";
+const ressourceBlocker = blockResourcesPlugin({
+  blockedTypes: new Set(["image", "stylesheet"]),
+});
+puppeteer.use(ressourceBlocker);
 
-const fs = require("fs");
+import { Cluster } from "puppeteer-cluster";
 
-const { initDatabase } = require("./database/mongoConnector");
+import fs from "fs";
+
+import { initDatabase } from "./database/mongoConnector";
+import { getRestaurantData } from "./restaurants/extractor";
+import { getReviewData } from "./reviews/extractor";
 
 process.on("unhandledRejection", (error, p) => {
   console.log("=== UNHANDLED REJECTION ===");
   console.dir(error);
 });
 
-// Selectors
+const baseURL = "https://www.tripadvisor.de/";
 const nextButtonSelector = ".nav.next";
-
-const reviewSelector = ".reviewSelector";
-
-const reviewStartsSelector = ".ui_bubble_rating";
-const reviewTitleSelector = ".noQuotes";
-const reviewTranslateButtonSelector =
-  ".prw_reviews_google_translate_button_hsx";
-const reviewTextSelector = ".entry";
-const reviewRatingDataSelector = ".ratingDate";
-const reviewVisitDataSelector = ".stay_date_label";
-const reviewMobileSelector = ".viaMobile";
-const reviewThumbsUpSelector = ".numHelp.emphasizeWithColor";
-
-const nameSelector = 'h1[data-test-target="top-info-header"]';
-const adressSelector = 'a[href="#MAPVIEW"]';
-const phoneSelector = 'a[href^="tel:"]';
-const emailSelector = 'a[href^="mailto:"]';
-const websiteXSelector = '//a[text()="Webseite"]';
-const reviewsSelector = 'a[href="#REVIEWS"] > span';
-const generalStarsXSelector = '//div[text()="GESAMTWERTUNGEN"]';
-const starsXSelector = '//div[text()="GESAMTWERTUNGEN"]';
-const priceRangeXSelector = '//div[text()="PREISSPANNE"]';
-const kitchenXSelector = '//div[text()="KÜCHEN"]';
-const descriptionXSelector = '//div[text()="Beschreibung"]';
-const otherDietsXSelector = '//div[text()="ANDERE ERNÄHRUNGSFORMEN"]';
-const mealsXSelector = '//div[text()="MAHLZEITEN"]';
-const otherFunctionsXSelector = '//div[text()="FUNKTIONEN"]';
-const ratingDistributionSelector = 'div[data-param="trating"]';
-const ratingItemSelector = ".item";
-const allLanguages =
+const reviewLinkSelector = 'a[href^="/ShowUserReview"]';
+export const allLanguages =
   ".content > .choices #filters_detail_language_filterLang_ALL";
-const allLanguagesSelected =
+export const allLanguagesSelected =
   '.content > .choices #filters_detail_language_filterLang_ALL[checked="checked"]';
-
-const allDetailsSelector = '//a[text()="Alle Details anzeigen"]';
-
-const getRestaurantData = async (page) => {
-  const name = await page.$eval(nameSelector, (e) => e.innerHTML);
-  const address = await page.$eval(adressSelector, (e) => e.innerHTML);
-  const phone = await page.$eval(phoneSelector, (e) => e.innerHTML);
-  const email = await page.$eval(emailSelector, (e) =>
-    e.href.replace("mailto:", "").replace("?subject=?", "")
-  );
-  const website = await page
-    .$x(websiteXSelector)
-    .then((items) => items[0].evaluate((e) => e.href));
-  const reviews = await page.$eval(reviewsSelector, (e) =>
-    e.innerText.replace(/\D/g, "")
-  );
-  // const stars = await page
-  //   .$x(starsXSelector)
-  //   .then((node) => node[0].evaluate((item) => item.innerHTML));
-  const priceRange = await page
-    .$x(priceRangeXSelector)
-    .then((items) =>
-      items[0].evaluate((e) => e.parentNode.lastChild.innerText)
-    );
-  const kitchen = await page
-    .$x(kitchenXSelector)
-    .then((items) => items[0].evaluate((e) => e.parentNode.lastChild.innerText))
-    .then((str) => str.split(",").map((item) => item.trim()));
-  // const description = await page
-  //   .$x(descriptionXSelector)
-  //   .then((items) => items[0].evaluate((e) => e.innerText));
-  // const otherDiets = await page
-  //   .$x(otherDietsXSelector)
-  //   .then((items) => items[0].evaluate((e) => e.innerText));
-  // const meals = await page
-  //   .$x(mealsXSelector)
-  //   .then((items) => items[0].evaluate((e) => e.innerText));
-  // const otherFunctions = await page
-  //   .$x(otherFunctionsXSelector)
-  //   .then((items) => items[0].evaluate((e) => e.innerText));
-
-  await page.waitForSelector(allLanguages);
-  await page.click(allLanguages);
-  await page.waitForSelector(allLanguagesSelected);
-
-  const ratings = await page
-    .$(ratingDistributionSelector)
-    .then((e) =>
-      e.$$eval(ratingItemSelector, (nodes) =>
-        nodes.map((node) => node.lastChild.innerHTML)
-      )
-    );
-
-  console.log(address);
-  console.log(
-    address.trim().split(",").slice(-1)[0].replace(" Deutschland", "")
-  );
-
-  return {
-    url: page.url(),
-    name,
-    // description,
-    reviews,
-    // stars: {
-    //   general: Number,
-    //   kitchen: Number,
-    //   service: Number,
-    //   quality: Number,
-    //   general: Number,
-    // },
-    phone,
-    address,
-    email,
-    city: address.trim().split(",").slice(-1)[0].replace(" Deutschland", ""),
-    zipCode: address.match(/\d{5}/g)[0],
-    website,
-    kitchen,
-    priceRange,
-    // otherDiets,
-    // meals: [String],
-    // otherFunctions: [String],
-    ratingDistribution: {
-      excellent: ratings[0],
-      verygood: ratings[1],
-      good: ratings[2],
-      fair: ratings[3],
-      poor: ratings[4],
-    },
-    crawled: true,
-  };
-};
-
-const getReviews = (page, id) =>
-  page.$$eval(reviewLinkSelector, (nodes) =>
-    nodes.map((node) => extractReviewInfo(node, id))
-  );
-
-const getReviewData = async (element, id) => {
-  const languageRegex = /sl=(\w{2})&/;
-
-  const reviewId = element.dataset.reviewid;
-  const stars = await element.$eval(
-    reviewStartsSelector,
-    (node) => node.getAttribute("alt")[0]
-  );
-  const title = await element.$eval(
-    reviewTitleSelector,
-    (node) => node.innerHTML
-  );
-  const text = await element.$eval(
-    reviewTextSelector,
-    (node) => node.innerText
-  );
-  const language =
-    (await element.$eval(
-      reviewTranslateButtonSelector,
-      (node) => languageRegex.exec(node.firstChild.dataset.url)[0]
-    )) || "de";
-  const reviewDate = await element.$eval(ratingDate, (node) =>
-    Date.parse(node.title)
-  );
-  const visitDate = await element.$eval(reviewVisitDataSelector, (node) =>
-    Date.parse(node.parentElement.innerText.slice(14))
-  );
-  const mobile = await element.$(reviewMobileSelector);
-  const thumbsUp = await element.$eval(reviewThumbsUpSelector, (node) =>
-    node.innerHTML.trim()
-  );
-
-  const user = getUserFromUserId(userId);
-
-  return {
-    restaurantId: id,
-    reviewId,
-    stars,
-    title,
-    text,
-    language,
-    reviewDate,
-    visitDate,
-    mobile: !!mobile,
-    thumbsUp,
-    // postedBy: { type: ObjectId, ref: "User" },
-  };
-};
-
-const getUserData = async (page) => {};
 
 (async () => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_BROWSER,
-    maxConcurrency: 2,
+    maxConcurrency: 4,
     monitor: true,
     timeout: 3600 * 2000,
     puppeteer,
@@ -229,21 +65,28 @@ const getUserData = async (page) => {};
     updateRestaurant,
     insertManyReviews,
     updateReview,
-    updateUser,
+    getAllUncrawledReviews,
   } = await initDatabase();
 
-  const crawlRestaurant = await cluster.task(
-    async ({ page, data: restaurant }) => {
-      await page.goto(restaurant.url);
+  const crawlRestaurant = async ({ page, data: restaurant }) => {
+    await page.goto(restaurant.url);
 
-      const restaurantData = await getRestaurantData(page);
-      await updateRestaurant(restaurant._id, restaurantData);
-      checkIfErrorUrlSuccessful(restaurant);
+    const restaurantData = await getRestaurantData(page);
 
-      const reviews = Set();
+    let reviews = new Set();
+    let newReviews = await page.$$eval(reviewLinkSelector, (nodes) =>
+      nodes.map((node) => node.href)
+    );
+    newReviews.forEach((url) =>
+      reviews.add({ url: url, restaurantId: restaurant._id })
+    );
 
-      let newReviews = await getReviews(page, restaurant._id);
+    if (restaurantData.reviews > 10) {
+      await page.waitForSelector(allLanguages);
+      await page.click(allLanguages);
+      await page.waitForSelector(allLanguagesSelected);
 
+      await page.waitForSelector(nextButtonSelector);
       let nextButtonUrl = await page.$eval(
         nextButtonSelector,
         (node) => node.href
@@ -251,38 +94,37 @@ const getUserData = async (page) => {};
 
       while (nextButtonUrl) {
         await page.goto(nextButtonUrl);
-        urls = await getRestaurantUrls(page);
-        urls.forEach((url) => restaurants.add(url));
+        newReviews = await page.$$eval(reviewLinkSelector, (nodes) =>
+          nodes.map((node) => node.href)
+        );
+        newReviews.forEach((url) =>
+          reviews.add({ url: url, restaurantId: restaurant._id })
+        );
+        await page.waitForSelector(nextButtonSelector);
         nextButtonUrl = await page.$eval(
           nextButtonSelector,
           (node) => node.href
         );
       }
-
-      restaurants = [...restaurants].map((item) => ({
-        url: baseURL + item,
-      }));
-
-      reviews = [...reviews];
-      insertManyReviews(reviews);
     }
-  );
 
-  const crawlReview = await cluster.task(async ({ page, data: review }) => {
+    reviews = [...reviews].map((item) => ({
+      url: baseURL + item.url,
+      ...item,
+    }));
+
+    const createdReviews = await insertManyReviews(reviews);
+    createdReviews.forEach((item) => cluster.queue(item, crawlReview));
+    await updateRestaurant(restaurant._id, restaurantData);
+    checkIfErrorUrlSuccessful(restaurant);
+  };
+
+  const crawlReview = async ({ page, data: review }) => {
     await page.goto(review.url);
-
-    const reviewData = await getReviewData(page, review.restaurantId);
+    const reviewData = await getReviewData(page, review._id);
     await updateReview(review._id, reviewData);
     checkIfErrorUrlSuccessful(review);
-  });
-
-  const crawlUser = await cluster.task(async ({ page, data: user }) => {
-    await page.goto(user.url);
-
-    const userData = await getUserData(page);
-    await updateUser(user._id, userData);
-    checkIfErrorUrlSuccessful(user);
-  });
+  };
 
   cluster.on("taskerror", (err, data, willRetry) => {
     if (willRetry) {
@@ -295,12 +137,14 @@ const getUserData = async (page) => {};
         errors[data.url] = [err.message];
       }
     } else {
-      console.error(`Failed to crawl ${data}: ${err.message}`);
+      console.error(`Failed to crawl ${data}: ${err}`);
     }
   });
 
-  const uncrawled = await getAllUncrawledRestaurant();
-  uncrawled.forEach((item) => cluster.queue(item, crawlRestaurant));
+  const uncrawledRestaurants = await getAllUncrawledRestaurant();
+  uncrawledRestaurants.forEach((item) => cluster.queue(item, crawlRestaurant));
+  const uncrawledReviews = await getAllUncrawledReviews();
+  uncrawledReviews.forEach((item) => cluster.queue(item, crawlReview));
 
   let data = JSON.stringify(errors);
   fs.writeFileSync("data/crawlerErrors.json", data);
